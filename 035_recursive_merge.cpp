8ff8f51a433caa48f68a2099dcb707f329c836fa
sinchanarokkad <sinchanarokkas2005@gmail.com>
2023-12-04 20:16:43 +0530

    modified

--- 035_recursive_merge.cpp
+++ 035_recursive_merge.cpp
@@ -39,14 +39,14 @@ void Merge(int A[], int low, int mid, int high){
  
 void RecursiveMergeSort(int A[], int low, int high){
     if (low < high){
-        // Calculate mid point
+        
         int mid = low + (high-low)/2;
  
-        // Sort sub-lists
+      
         RecursiveMergeSort(A, low, mid);
         RecursiveMergeSort(A, mid+1, high);
  
-        // Merge sorted sub-lists
+        
         Merge(A, low, mid, high);
     }
 }
