8c55a7ed7be32fd6660a9e001f3fbf404f19e0ba
sinchanarokkad <sinchanarokkas2005@gmail.com>
2023-12-05 07:46:42 +0530

    complete

---
+++ 040_length_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    char S[] = "WELCOME";
+    int i;
+    for (i=0 ; S[i]!='\0';i++)
+    {
+
+    }
+    printf("length is %d",i);
+    return 0;
+}
