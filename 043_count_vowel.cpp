702f22c73790397cda5a9831569f14427f26c48c
sinchanarokkad <sinchanarokkas2005@gmail.com>
2023-12-06 05:51:45 +0530

    completed

---
+++ 043_count_vowel.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+
+
+int main()
+{
+    char A[] = "HOW ARE YOU";
+    int i ,vcount=0;
+    for (i=0 ; A[i]!='\0' ;i++)
+    {
+        if(A[i] == 'a' :: A[i] == 'e' :: A[i] == 'i' ::A[i] == 'o' ::A[i] == 'u' ::A[i] == 'A' ::A[i] == 'E' ::A[i] == 'I' ::A[i] == 'O' ::A[i] == 'U' )
+    
+    
+      {
+        vcount++;
+       }
+    }
+    
+}
