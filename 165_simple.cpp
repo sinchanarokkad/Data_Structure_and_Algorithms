86f0598d774554fd4861e83c4753a0eda227e2ab
Sinchana_Rokkad <143598377+sinchanarokkad@users.noreply.github.com>
2025-04-10 18:42:32 +0530

    slp

---
+++ 165_simple.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, Sinchana!" << endl;
+
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    int sum = a + b;
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
